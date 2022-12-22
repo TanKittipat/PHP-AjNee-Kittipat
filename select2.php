920070576471890a36881f292662a0116821608f
TanKittipat <kittipat12tan@gmail.com>
2022-12-22 10:38:24 +0700

    22/12/65 10:40 first commit

---
+++ select2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>select-2</title>
+</head>
+<body>
+    <?php
+    try {
+        require 'connect.php';
+        $sql = "SELECT * FROM customer";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        //แบบที่ 1
+        while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo '<br>' .
+                'รหัสลูกค้าของฉันแบบที่ 1 : ' .
+                $result['CustomerID'] .
+                ' วันเกิด : ' .
+                $result['Birthdate'] .
+                ' ยอดหนี้ : ' .
+                $result['OutstandingDebt'];
+        }
+
+    } catch (PDOException $e) {
+        echo 'ไม่สามารถประมวลผลข้อมูลได้ : ' . $e->getMessage();
+    }
+
+    $conn = null;
+    ?>
+
+</body>
+</html>
