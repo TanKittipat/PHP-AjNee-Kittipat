e0f31ffb75041b9c87d8c3603751cd6bacd9aefa
TanKittipat <kittipat12tan@gmail.com>
2023-01-06 16:24:59 +0700

    06/01/66 16:10

---
+++ select_injection_form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Customer and Test SQL Injection</title>
+</head>
+<body>
+    <h1>Test SQL Injection</h1>
+    <form action="select_injection.php" method="GET">
+    <input type="text" placeholder="Enter Customer ID" name="CustomerID">
+    <br><br>
+    <input type="submit">
+    </form>
+</body>
+</html>
